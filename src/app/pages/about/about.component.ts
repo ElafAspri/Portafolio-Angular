19204490b15d7a7010cc4262b8f7b90fe8a460ba
Eleazar Aranguren <eleazararangurensoto@gmail.com>
2023-03-07 18:55:49 -0400

    templeates listos

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { InfoPaginaService } from '../../services/info-pagina.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent  {
+
+  constructor( public infoService: InfoPaginaService) { }
+
+  ngOnInit() {
+
+  }
 
 }
